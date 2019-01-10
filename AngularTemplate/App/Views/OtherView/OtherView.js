900811229bb78ea45922aff939554892fc53dbbe
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2019-01-10 22:38:04 +0200

    added sweet alert

--- AngularTemplate/App/Views/OtherView/OtherView.js
+++ AngularTemplate/App/Views/OtherView/OtherView.js
@@ -4,5 +4,46 @@
     vm.Test = function () {
         alert("OtherView");
     };
+    vm.ShowSweetAlert = function () {
+        Swal(
+            'The Internet?',
+            'That thing is still around?',
+            'question'
+        );
+    };
+
+    vm.Show2SweetAlert = function () {
+        const swalWithBootstrapButtons = Swal.mixin({
+            confirmButtonClass: 'btn btn-success',
+            cancelButtonClass: 'btn btn-danger',
+            buttonsStyling: false
+        });
+
+        swalWithBootstrapButtons({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.value) {
+                swalWithBootstrapButtons(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                );
+            } else if (
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons(
+                    'Cancelled',
+                    'Your imaginary file is safe :)',
+                    'error'
+                );
+            }
+        });
+    };
 
 });
